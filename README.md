7756b1acaac56760eb1b8d05d4d465e99bb6cb92
Jurgen <jurgen.geitner@gmail.com>
2024-08-15 10:34:32 +0200

    log all request headers

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # simple-express-server
 
 ## About
+
 A project for me to learn about [Express](https://expressjs.com/). 
 Thanks to [Flavio Copes](https://flaviocopes.com/) for the [tutorial](https://thevalleyofcode.com/express).
 
 ## Usage
 
+```node index.js```
 
 ## License
+
 MIT
--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    console.log(req.headers)
+    res.send('Hello World!');
+
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000');
 });
 
